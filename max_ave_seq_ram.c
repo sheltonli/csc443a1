a2b885f7859c7c8c5ef2d790826a0a2ff6c5c1ed
sheltonli <shelton.li.ts@gmail.com>
2016-01-29 13:32:20 -0500

    Updated with timing, fixed so max and average is calculated

--- max_ave_seq_ram.c
+++ max_ave_seq_ram.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/time.h>
 
 typedef struct max_avg{
-  int avg;
+  float avg;
   int max;
 } Max_average;
 
@@ -12,7 +13,9 @@ typedef struct record {
   int uid2;
 } Record;
 
-Max_average max_ave_seq_ram(FILE * fp_read);
+const int MB = 1024 * 1024;
+
+void max_ave_seq_ram(FILE * fp_read);
 /* allocate buffer for 1 block */
 
 int main(int argc, char* argv[]){
@@ -33,59 +36,72 @@ int main(int argc, char* argv[]){
   return 0;
 }
 
-Max_average max_ave_seq_ram(FILE * fp_read){
+void max_ave_seq_ram(FILE * fp_read){
   fseek(fp_read, 0, SEEK_END);
   long fsize = ftell(fp_read);
   fseek(fp_read, 0, SEEK_SET);
-
-  printf("%lu\n", fsize);
 
   Record * buffer = (Record *) calloc (fsize, sizeof(Record));
   /* read records into buffer */
   Max_average ma;
   int current_conns = 0; 
   int max_conns = 0;
-  int total_conns = 0;
+  float total_conns = 0.0;
   int current_id= -1;
   int last_id = -1;
-  int total_ids = 0;
+  float total_ids = 0.0;
 
   int result = fread (buffer, sizeof(Record), fsize, fp_read);
   int count = 0;
 
+  clock_t begin, end;
+  double time_spent;
+  long total_records = 0;
+
+  begin = clock();
   /* Go through each block */
   for(int i = 0; i < fsize; i++){
-    count++;
     /* First loop */
-    // if (current_id == -1){
-    //   current_id = buffer[0].uid1;
-    //   last_id = current_id;
-    // }
-    /*  */ 
+    if (current_id == -1){
+      current_id = buffer[0].uid1;
+      last_id = current_id;
+    }
+
     current_id = buffer[i].uid1;
     if (current_id == 0){
       break;
     }
-    printf("current_id: %d\n", current_id);
-//     if (current_id == last_id){
-//       current_conns ++;
-//     }
-//     else{
-//       total_ids ++;
-//       last_id = current_id;
-//       printf("Switch of ids. last_id: %d has %d connections. \t current_id: %d \n", last_id, current_conns, current_id);
-//       if (current_conns > max_conns){
-//         max_conns = current_conns;
-//         current_conns = 1;
-//       }
-//     }
-   }
-   printf("%d\n", count);
-//   total_conns ++;
-//   result = fread (buffer, sizeof(Record), records_per_block, fp_read);
 
-//  ma.avg = total_conns/total_ids;
-//  ma.max = max_conns;
-//  printf("Average number of connections: %d \t; Maximum number of connections: %d \n", ma.avg, ma.max);
-  return ma;
+    count++;
+
+    /* Remove later */
+    //printf("current_id: %d\n", current_id);
+
+    if (current_id == last_id){
+      current_conns ++;
+    }
+    else {
+      total_ids ++;
+      last_id = current_id;
+//       printf("Switch of ids. last_id: %d has %d connections. \t current_id: %d \n", last_id, current_conns, current_id);
+      if (current_conns > max_conns){
+        max_conns = current_conns;
+        current_conns = 1;
+      }
+    }
+    total_conns ++;
+    total_records ++;
+  }
+  end = clock();
+
+  /* Remove later */
+  // printf("%d\n", count);
+
+  ma.avg = total_conns/total_ids;
+  ma.max = max_conns;
+  printf("Average number of connections: %.3f \t; Maximum number of connections: %d \n", ma.avg, ma.max);
+
+  time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+  printf ("Data rate: %.3f MBPS\n", ((total_records*sizeof(Record))/time_spent)/MB);
+
 }
