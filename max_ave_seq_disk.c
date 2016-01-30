8472e30eba465d86dbf9752ec69c95868e978ed8
Valery <valeryzavalna@gmail.com>
2016-01-29 23:25:29 -0500

    counts followers correctly, but accesses some invalid ids for some reson...

--- max_ave_seq_disk.c
+++ max_ave_seq_disk.c
@@ -12,7 +12,7 @@ typedef struct record {
   int uid2;
 } Record;
 
-void max_ave_seq_disk(FILE * fp_read, int block_size);
+Max_average max_ave_seq_disk(FILE * fp_read, int block_size);
 /* allocate buffer for 1 block */
 
 int main(int argc, char* argv[]){
@@ -36,7 +36,7 @@ int main(int argc, char* argv[]){
   return 0;
 }
 
-void max_ave_seq_disk(FILE * fp_read, int block_size){
+Max_average max_ave_seq_disk(FILE * fp_read, int block_size){
   Max_average ma;
   int records_per_block = block_size/sizeof(Record);
   Record * buffer = (Record *) calloc (records_per_block, sizeof(Record));  
@@ -53,33 +53,33 @@ void max_ave_seq_disk(FILE * fp_read, int block_size){
   int i = 0;
   while(result == records_per_block){
     /* Go through each block */
-    for(int i = 0; i < block_size; i +=sizeof(Record)){
+    for(int i = 0; i < block_size; i ++){
       /* First loop */
-      // if (current_id == -1){
-      //   current_id = buffer[0].uid1;
-      //   last_id = current_id;
-      // }
-      /*  */ 
+      if (current_id == -1){
+        current_id = buffer->uid1;
+        last_id = current_id;
+      }
       current_id = (buffer + i)->uid1;
-      printf("current_id: %d\n", current_id);
-  //     if (current_id == last_id){
-  //       current_conns ++;
-  //     }
-  //     else{
-  //       total_ids ++;
-  //       last_id = current_id;
-  //       printf("Switch of ids. last_id: %d has %d connections. \t current_id: %d \n", last_id, current_conns, current_id);
-  //       if (current_conns > max_conns){
-  //         max_conns = current_conns;
-  //         current_conns = 1;
-  //       }
-  //     }
-       }
-  //   total_conns ++;
-  //   result = fread (buffer, sizeof(Record), records_per_block, fp_read);
+      //printf("current_id: %d\n", current_id);
+      if (current_id == last_id){
+        current_conns ++;
+      }
+      else{
+        printf("Switch of ids\n");
+        printf("id: %d has %d connections\n", last_id, current_conns);
+        total_ids ++;
+        last_id = current_id;
+        if (current_conns > max_conns){
+          max_conns = current_conns;
+        }
+        current_conns = 1;
+      }
     }
-  // ma.avg = total_conns/total_ids;
-  // ma.max = max_conns;
-  // printf("Average number of connections: %d \t; Maximum number of connections: %d \n", ma.avg, ma.max);
-  // return ma;
+    total_conns ++;
+    result = fread (buffer, sizeof(Record), records_per_block, fp_read);
+  }
+  ma.avg = total_conns/total_ids;
+  ma.max = max_conns;
+  printf("Average number of connections: %d \t; Maximum number of connections: %d \n", ma.avg, ma.max);
+  return ma;
 }
